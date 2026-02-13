fc59771b7c350c216dbc21420f65b75705cd251d
syedaumme <kulsumsyedaumme00@gmail.com>
2026-02-13 12:45:14 -0500

    Realtime Data Snapshot

--- scripts/cpu_monitor.sh
+++ scripts/cpu_monitor.sh
@@ -9,6 +9,7 @@ top_cpu=$(ps -eo %cpu --sort=-%cpu | head -n 2 | tail -n 1 | cut -d. -f1)
 echo -e "\n ---------------------------- C P U   U S A G E   C H E C K -----------------------------------\n">>"$LOG_FILE"
 
 echo -e "\n [$(date '+%Y-%m-%d %H:%M:%S')]\n" >>"$LOG_FILE"
+
 if [ "$top_cpu" -gt "$THRESHOLD" ]; then
     echo "WARNING: High CPU usage detected (${top_cpu}%)" >>"$LOG_FILE"
 EXIT_CODE=1
@@ -21,4 +22,15 @@ echo -e "\n---------------------------------------------------------------------
 echo -e "\nTop CPU-consuming processes:\n [$(date '+%Y-%m-%d %H:%M:%S')] \n ">>"$LOG_FILE"
 ps -eo pid,comm,%cpu --sort=-%cpu | head>>"$LOG_FILE"
 
+
+# Summary file / Real time snapshot
+
+SUMMARY_FILE="../logs/cpu_summary.txt"
+echo "---------------- CPU USAGE SUMMARY ----------------" > "$SUMMARY_FILE"
+echo "Last Checked : $(date '+%Y-%m-%d %H:%M:%S')" >> "$SUMMARY_FILE"
+echo "CPU Usage : ${top_cpu}%" >> "$SUMMARY_FILE"
+echo >> "$SUMMARY_FILE"
+
+echo "Top CPU-consuming processes:" >> "$SUMMARY_FILE"
+ps -eo pid,comm,%cpu --sort=-%cpu | head >> "$SUMMARY_FILE"
 exit $EXIT_CODE
